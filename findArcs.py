b9a2507afafaa2d6b5b8c4c5d7b1f81fddd4edb2
lahmann <lahmann@mit.edu>
2019-09-06 08:58:13 -0400

    Added a functioning MATLAB script

--- findArcs.py
+++ findArcs.py
@@ -25,6 +25,11 @@ destDir,movieFile = os.path.split(sys.argv[1])
 print('destination:',os.path.join(destDir,'{:s}_{:d}.png'.format(movieFile.split('.')[-2],1)))
 assert len(destDir) == 0 or os.path.exists(destDir)
 
+print(os.path.join(destDir,movieFile))
+print('');
+print(movieFile)
+print('')
+
 reader = imageio.get_reader(os.path.join(destDir,movieFile))#,fps=30/3.)
 fps = reader.get_meta_data()['fps']
 print('fps:',fps)
@@ -38,7 +43,7 @@ def isEvent(brightness):
 def isDuplicate(brightness):
 	return (brightness[-1] < MAGIC_DUPLICATE_RATIO * brightness[-2] and \
 			brightness[-1] > brightness[-2] / MAGIC_DUPLICATE_RATIO)
-	
+
 def spliceImages(im1,im2):
 	imResult = np.maximum(im1,im2)
 	return imResult
@@ -49,12 +54,12 @@ currentEventList = []
 inds=[]
 brightness=[]
 for i,im in enumerate(reader):
-	if i%100 == 0: 
+	if i%100 == 0:
 		print('Frame',i) # for sanity
 	inds.append(i)
 	b = np.sum(im)*1.0/np.size(im)/256./3.
 	brightness.append(b)	# build brighness list (helpful for arc identification
-	
+
 	# identify event
 	if isEvent(brightness): # we have an event
 		# reject duplicates
@@ -66,7 +71,7 @@ for i,im in enumerate(reader):
 			currentEventList.append(eventFrameDict)
 			# make sure the next iteration knows previous loop is event
 			eventLastLoop = True
-	
+
 	elif eventLastLoop: # no longer in an event, but last loop was an event
 		eventList.append(currentEventList)
 		currentEventList = []
@@ -74,12 +79,12 @@ for i,im in enumerate(reader):
 
 for ai,e in enumerate(eventList):
 	print('Arc at: {:0.1f} s'.format(e[0]['i']/fps))
-	
+
 	if len(e) == 1:		#if less than two pick, the one!
 		splicedIm = e[0]['image']
 	elif len(e) == 2:	# combine the two images of the event
 		splicedIm = spliceImages(e[0]['image'],e[1]['image'])
 	else:	# if more than two, pick brightest two
 		pass
-	
+
 	imageio.imwrite(os.path.join(destDir,'{:s}_{:d}.png'.format(movieFile.split('.')[-2],1)),splicedIm)
